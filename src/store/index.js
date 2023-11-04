a65a4e4e24d9443d7b628a15836944571f8239cf
Paul Jor <jorpaulvincent@gmail.com>
2023-11-04 23:42:29 +0400

    update store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ import * as getters from "./getters";
 import * as mutations from "./mutations";
 import * as actions from "./actions";
 
-export const store = new Vuex.Store({
+export default new Vuex.Store({
     state,
     getters,
     mutations,
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,10 @@
-<template></template>
-
-<!--<template>
+<template>
     <div>
         <h4>Dashboard</h4>
+        <br />
+        <br />
+        <span>{{ $route.params.user_id }}</span>
         <mt-search v-model="search"></mt-search>
-        <div v-for="(item, index) in getAllUsers" :key="index">
-            <div>
-                {{ item }}
-            </div>
-        </div>
     </div>
 </template>
 
@@ -19,21 +15,5 @@ export default {
             search: "",
         };
     },
-
-    computed: {
-        getAllUsers() {
-            return this.$store.getters.fetchUsers;
-        },
-
-        filterSearch() {
-            const results = ["a", "b", "c", "d", "e", "f"];
-            return results;
-        },
-    },
-
-    created() {
-        this.$store.dispatch("fetchUsers");
-    },
 };
 </script>
--->
